7ee8897734491f90a1fa6ceca6eef110106b27c2
Cesahz <espinolac999@gmail.com>
2026-04-29 22:21:49 -0300

    chore: project structure and express setup

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const path = require('path')
+require('dotenv').config()
+
+const app = express()
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Template engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+// Database connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err))
+
+// Server
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
