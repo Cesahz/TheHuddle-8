e5a77b77739a3e77b64fcf0e4aff3e8bde8c1f35
Cesahz <espinolac999@gmail.com>
2026-05-11 11:25:25 -0300

    refactor:agregar comentarios mas detallados

--- src/routes/topicRoutes.js
+++ src/routes/topicRoutes.js
@@ -3,13 +3,13 @@ const router = express.Router()
 const topicController = require('../controllers/topicController')
 
 //nota, las rutas fijas siempre antes que las dinamicas, el orden importa
-router.get('/', topicController.getAllTopics)
-router.get('/topics/new', topicController.newTopicForm)
-router.post('/topics', topicController.createTopic)
-router.get('/topics/:id', topicController.getTopic)
-router.get('/topics/:id/edit', topicController.editTopicForm)
-router.put('/topics/:id', topicController.updateTopic)
-router.delete('/topics/:id', topicController.deleteTopic)
-router.post('/topics/:id/vote', topicController.voteTopic)
+router.get('/', topicController.getAllTopics) //lista de temas
+router.get('/topics/new', topicController.newTopicForm) //formulario crear
+router.post('/topics', topicController.createTopic) //crear tema
+router.get('/topics/:id', topicController.getTopic) //detalle del tema
+router.get('/topics/:id/edit', topicController.editTopicForm) //editar
+router.put('/topics/:id', topicController.updateTopic) //actualizar
+router.delete('/topics/:id', topicController.deleteTopic) //borrar
+router.post('/topics/:id/vote', topicController.voteTopic) //votar
 
 module.exports = router
